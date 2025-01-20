23baa7edea435d52c51042a6b9ff7f3f8741a477
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-21 00:29:09 +0800

    I have set the Pov of the self to a black screen

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ class Game:
         pg.display.flip()
         self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
