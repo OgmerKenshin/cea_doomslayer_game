1dad7a1a87114f79532bc18c8d897daf94492ebd
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-21 00:13:17 +0800

    I have set the basic settings for the POV of the self

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from settings import *
 
 class Game:
     def _init__(self):
-        
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+
+    def new_game(self):
+        pass
