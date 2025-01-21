8730fdcfcd0cf1e6e75bfd9c956eefacbd5d94dc
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-21 21:08:57 +0800

    this is the calculations needed for the base depth

--- raycasting.py
+++ raycasting.py
@@ -14,6 +14,17 @@ class RayCasting:
 
         ray_angle = self.game.player.angle - HALF_FOV + 0.0001
         for ray in range(NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+            depth_vert = (x_vert - ox) /cos_a
+            y_vert = oy + depth_vert * sin_a
+
+            delta_depth = dx / cos_a
+            dy = delta_depth * sin_a
+
+            
             ray_angle += DELTA_ANGLE
 
     def update(self):
