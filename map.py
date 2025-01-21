76b94def35abb0f1e8a55582675c9434043d7406
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-21 17:07:58 +0800

    initialized world map

--- map.py
+++ map.py
@@ -18,3 +18,9 @@ class Map:
         self.mini_map = mini_map
         self.world_map = {}
         self.get_map()
+
+    def get_map(self):
+        for j, row in enumerate(self.mini_map):
+            for i, value in enumerate(row):
+                if value:
+                    self.world_map[(i, j)] = value
