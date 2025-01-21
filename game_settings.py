b4bdacde52a9d174b6669e96b662b92d2a8dec48
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-21 20:53:08 +0800

    I accounting for ray casting in the game settings

--- game_settings.py
+++ game_settings.py
@@ -1,7 +1,18 @@
+import math
+
+
+
 RES = WIDTH, HEIGHT = 1800, 1000
 FPS = 60
 
 PLAYER_POS = 1.5, 5 #dis is where u will be when u spawn
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
-PLAYER_ROT_SPEED = 0.002
+PLAYER_ROT_SPEED = 0.05
+
+FOV = math.pi / 3
+HALF = FOV / 2
+NUM_RAYS = WIDTH //2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
