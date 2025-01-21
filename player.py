9e49bd06787b9b3d34a0993432b9fee17b119b27
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-21 18:07:48 +0800

    plugging in the players features

--- player.py
+++ player.py
@@ -1,3 +1,9 @@
 from game_settings import *
 import pygame as pg
 import math
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
