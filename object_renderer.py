be07cfd90a8f88ac4b3d30c6b081791663f119af
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-23 22:05:32 +0800

    injecting the texture into the map

--- object_renderer.py
+++ object_renderer.py
@@ -11,3 +11,12 @@ class ObjectRenderer:
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pg.image.load(path).convert_alpha()
         return pg.transform.scale(texture, res)
+    
+    def load_wall_textures(self):
+        return{
+            1: self.get_texture('txture_packs/1.png'),
+            2: self.get_texture('txture_packs/2.png'),
+            3: self.get_texture('txture_packs/3.png'),
+            4: self.get_texture('txture_packs/4.png'),
+            5: self.get_texture('txture_packs/6.png'),
+        }
