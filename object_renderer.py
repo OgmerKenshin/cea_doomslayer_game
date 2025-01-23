161e818c5e91b27faf79739668264a4b203fc278
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-23 21:53:42 +0800

    this block of code will load the texture pack and return and scale it to the walls

--- object_renderer.py
+++ object_renderer.py
@@ -6,3 +6,8 @@ class ObjectRenderer:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+
+    @staticmethod
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+        texture = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(texture, res)
