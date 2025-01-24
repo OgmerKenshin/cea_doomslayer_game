e751fdd6c5f9e736edab57422c36d195f9a86549
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-24 23:07:02 +0800

    making the mouse cursor not visible in game

--- player.py
+++ player.py
@@ -56,7 +56,7 @@ class Player:
         pg.draw.circle(self.game.screen, "green", (self.x * 100, self.y * 100), 15)
 
     def mouse_control(self):
-        mx, my = pg.mouse.get.get_pose()
+        mx, my = pg.mouse.get_pos()
         if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
             pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
         self.rel = pg.mouse.get_rel()[0]
