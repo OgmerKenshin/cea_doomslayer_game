48f54e4073978e6cb5109f61e3989c8f92484b89
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-24 23:59:58 +0800

    loading in the first sprite in the game

--- sprite_object.py
+++ sprite_object.py
@@ -1,3 +1,11 @@
 import pygame as pg
 from game_settings import *
 
+class SpriteObjects:
+    def __init__(self, game, path="static_sprites/chair.png", pos=(10.5, 3.5)):
+        self.game = game
+        self.player = game.player
+        self.x, self.y = pos
+        self.image = pg.image.load(path).convert_alpha()
+        self.IMAGE_WIGTH = self.image.get_width()
+        self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
