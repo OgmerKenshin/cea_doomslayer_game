66affb1fe6b200d96f61a2fd6f16212165ea0c76
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-24 23:35:53 +0800

    new method of the floor, the last method loaded half the screen as gray

--- object_renderer.py
+++ object_renderer.py
@@ -13,22 +13,20 @@ class ObjectRenderer:
     def draw(self):
         self.draw_background()
         self.render_game_objects()
-        self.draw_floor()
+        
 
     def draw_background(self):
         self.sky_offset = (self.sky_offset + 4.0 * self.game.player.rel) % WIDTH
         self.screen.blit(self.sky_image, (-self.sky_offset, 0))
         self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+        #i forgor the floor so here it is
+        pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
 
 
     def render_game_objects(self):
         list_objects = self.game.raycasting.objects_to_render
         for depth, image, pos in list_objects:
             self.screen.blit(image, pos)
-
-    def draw_floor(self):
-        floor_color = (30, 30, 30)
-        pg.draw.rect(self.screen, floor_color, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
 
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
