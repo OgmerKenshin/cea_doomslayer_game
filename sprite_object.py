7e0f808168b78019f2672a3f0eb34457879fa05e
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-25 00:04:57 +0800

    calculations for raycasting the objects

--- sprite_object.py
+++ sprite_object.py
@@ -11,7 +11,10 @@ class SpriteObjects:
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
 
     def get_sprite():
-        pass
+        dx = self.x - self.player.x
+        dy = self.y - self.player.y
+        self.dx, self.dy = dx, dy
+        self.theta = math.atan2(dy, dx)
 
     def update(self):
         self.get_sprite()
