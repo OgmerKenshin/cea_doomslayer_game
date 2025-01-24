4331f73f3f2bd19c548076745be330a83092a537
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-25 01:04:29 +0800

    this is to resize the object

--- sprite_object.py
+++ sprite_object.py
@@ -2,7 +2,8 @@ import pygame as pg
 from game_settings import *
 
 class SpriteObjects:
-    def __init__(self, game, path="static_sprites/candlebra.png", pos=(10.5, 3.5)):
+    def __init__(self, game, path="static_sprites/candlebra.png", 
+                 pos=(10.5, 3.5), scale=0.5, shift=0.0):
         self.game = game
         self.player = game.player
         self.x, self.y = pos
@@ -12,9 +13,11 @@ class SpriteObjects:
         self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height()
         self.dxm, self.dy, self.theta, self.screen_x, self.dist, self.norm_dist = 0, 0, 0, 0, 1, 1
         self.sprite_half_width = 0
+        self.SPRITE_SCALE = scale
+        self.SPRITE_HEIGHT_SHIFT = shift
 
     def get_sprite_projection(self):
-        proj = SCREEN_DIST / self.norm_dist
+        proj = SCREEN_DIST / self.norm_dist * self.SPRITE_SCALE
         proj_width, proj_height = proj * self.IMAGE_RATIO, proj
 
         image = pg.transform.scale(self.image, (proj_width, proj_height))
