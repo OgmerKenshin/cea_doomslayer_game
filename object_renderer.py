4ab5abe9f10016e0b6c41a9a903cb3802f931090
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-24 23:28:02 +0800

    the floor color was not rendering so I added a new method specifically for it

--- object_renderer.py
+++ object_renderer.py
@@ -11,7 +11,9 @@ class ObjectRenderer:
         self.sky_offset = 0
 
     def draw(self):
+        self.draw_background()
         self.render_game_objects()
+        self.draw_floor()
 
     def draw_background(self):
         self.sky_offset = (self.sky_offset + 4.0 * self.game.player.rel) % WIDTH
@@ -23,6 +25,10 @@ class ObjectRenderer:
         list_objects = self.game.raycasting.objects_to_render
         for depth, image, pos in list_objects:
             self.screen.blit(image, pos)
+
+    def draw_floor(self):
+        floor_color = (30, 30, 30)
+        pg.draw.rect(self.screen, floor_color, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
 
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
