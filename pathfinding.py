9730c659daab4315f831c1a4ec30701b59b409b0
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-27 22:56:06 +0800

    this will list down the adjacent tiles

--- pathfinding.py
+++ pathfinding.py
@@ -7,3 +7,12 @@ class Pathfinding:
         self.ways = [-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [1, -1], [-1, 1]
         self.graph = {}
         self.get_graph()
+
+    def get_mext_nodes(self, x, y):
+        return[(x + dx, y+ dy) for dx, dy in self.ways if (x + dx, y + dy) not in self.game.map.world_map]
+
+    def get_graph(self):
+        for y, row in enumerate(self.map):
+            for x, col in enumerate(row):
+                if not col:
+                    self.graph[(x, y)] = self.graph.get((x, y), []) + self.get_next_nodes(x, y)
