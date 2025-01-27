87f3ba4a851c8a9db9ca6c58d1f52370acbafc6b
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-27 18:26:41 +0800

    here I am centering the shotgun from my pov

--- weapon.py
+++ weapon.py
@@ -3,3 +3,7 @@ from sprite_object import *
 class weapon(AnimatedSprite):
     def __init__(self, game, path= "animated_sprites/shotgun/1pac.png", scale=0.4, animation_time=90):
         super().__init__(game=game, path=path, scale=scale, animation_time=animation_time)
+        self.images = deque(
+            [pg.transform.smoothscale(img, (self.image.get_with() * scale, self.image.get_height() * scale))
+             for img in self.images])
+        self.weapon_pos = (HALF_WIDTH - self.images[0].get_width() // 2, HEIGHT - self.images[0].get_height())
