5a4ebc050e6b74298ff7e62b805b7d9f8e727404
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-27 18:46:13 +0800

    this will draw the gun itself on to the screen

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from raycasting import *
 from object_renderer import *
 from sprite_object import *
 from object_handler import *
-
+from weapon import *
 
 class Game:
     def __init__(self):
@@ -24,20 +24,20 @@ class Game:
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
+        self.weapon = weapon(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
         self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        #self.screen.fill('black')
         self.object_renderer.draw()
-       #self.map.draw()
-       #self.player.draw()
+        self.weapon.draw()
 
     def check_events(self):
         for event in pg.event.get():
--- weapon.py
+++ weapon.py
@@ -4,12 +4,12 @@ class weapon(AnimatedSprite):
     def __init__(self, game, path= "animated_sprites/shotgun/1pac.png", scale=0.4, animation_time=90):
         super().__init__(game=game, path=path, scale=scale, animation_time=animation_time)
         self.images = deque(
-            [pg.transform.smoothscale(img, (self.image.get_with() * scale, self.image.get_height() * scale))
+            [pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale))
              for img in self.images])
         self.weapon_pos = (HALF_WIDTH - self.images[0].get_width() // 2, HEIGHT - self.images[0].get_height())
 
     def draw(self):
-        self.game.screen.blit(self..images[0], self.weapon_pos)
+        self.game.screen.blit(self.images[0], self.weapon_pos)
         
     def updat(self):
         pass
