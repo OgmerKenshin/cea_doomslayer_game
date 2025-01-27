e8a5ce69d771f317dcc3d117ac54763e5ca70176
OgmerKenshin <kenshinaogmer@gmail.com>
2025-01-27 20:20:41 +0800

    added method for animation of soldier npc

--- npc.py
+++ npc.py
@@ -10,3 +10,12 @@ class NPC(AnimatedSprite):
         self.idle_images = self.get_images(self.path + '/idle')
         self.pain_images = self.get_images(self.path + '/pain')
         self.walk_images = self.get_images(self.path + '/walk')
+
+        self.attack_dist = randint(3, 6)
+        self.speed = 0.03
+        self.size = 10
+        self.health = 100
+        self.attack_damage = 10
+        self.accuracy = 0.15
+        self.alive = True
+        self.pain = False
